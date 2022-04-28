7a17dcbb57a66c2c495209e544078fccd299d979
Ian Park <ianpark@Ianui-MacBookPro.local>
2022-04-28 17:27:38 +0900

    Add: Touchable Button

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <SafeAreaView style={styles.defaultFlex}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Text style={styles.buttonText}>Work</Text>
+          </TouchableOpacity>
+          <Pressable onPress={(e) => console.log(e.target)}>
+            <Text style={styles.buttonText}>Travel</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  defaultFlex: { flex: 1 },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 30,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "space-between",
+  },
+  buttonText: {
+    color: theme.grey,
+    fontSize: 32,
+    fontWeight: "600",
   },
 });
---
+++ colors.js
@@ -0,0 +1,5 @@
+export const theme = {
+  background: "#191B1E",
+  grey: "#3A3D40",
+  white: "#fefefe",
+};
