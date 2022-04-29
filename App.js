cf9eb5fa7ebd197f9ac16fcf4787d28a65f93274
Ian Park <ianpark@Ianui-MacBookPro.local>
2022-04-29 12:05:24 +0900

    Add: Implement painting Todos

--- App.js
+++ App.js
@@ -7,15 +7,24 @@ import {
   SafeAreaView,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
   const [isWorking, setIsWorking] = useState(true);
-  const [todoItem, setTodoItem] = useState("");
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setIsWorking(false);
   const work = () => setIsWorking(true);
-  const onChangeText = (payload) => setTodoItem(payload);
+  const onChangeText = (payload) => setText(payload);
+  const addTodo = () => {
+    if (text === "") return;
+    // save to do
+    const newToDos = { ...toDos, [Date.now()]: { text, isWorking } };
+    setText("");
+    setToDos(newToDos);
+  };
 
   return (
     <View style={styles.container}>
@@ -46,9 +55,21 @@ export default function App() {
         <TextInput
           placeholder={isWorking ? "Add a To Do" : "Where do you want to go"}
           onChangeText={onChangeText}
-          value={todoItem}
+          value={text}
           style={styles.input}
+          returnKeyType="done"
+          onSubmitEditing={addTodo}
         />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {Object.keys(toDos).map(
+            (key) =>
+              toDos[key].isWorking === isWorking && (
+                <View style={styles.toDo} key={key}>
+                  <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                </View>
+              )
+          )}
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
@@ -75,7 +96,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 16,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBackground,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  toDoText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: theme.white,
   },
 });
--- colors.js
+++ colors.js
@@ -2,4 +2,5 @@ export const theme = {
   background: "#191B1E",
   grey: "#3A3D40",
   white: "#fefefe",
+  toDoBackground: "#5c5c60",
 };
